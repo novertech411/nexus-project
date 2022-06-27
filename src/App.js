1abdf09da84f9fed8ecdb021dbd000c32f8f2614
shola eyinmosan <novertech4@gmail.com>
2022-06-27 14:08:56 -0500

    sidebar  complet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.scss';
 import { Routes,  Route } from 'react-router-dom';
 import Layout  from './components/layout';
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,23 @@
-import './index.scss';
+import { Outlet } from 'react-router-dom'
+import Sidebar from '../Sidebar/Sidebar'
+import './index.scss'
 
 const Layout = () => {
-    return <>hell</>
+  return (
+    <div className="app">
+      <Sidebar />
+
+      <div>
+        <span className="tage  top-tags">&lt;body&gt;</span>
+        <Outlet />
+        <span className="tag bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html"></span>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
